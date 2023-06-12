9bd6018b5db01873b00709d26d983944846bd3fc
Victor <victor.alves.sales@hotmail.com>
2023-06-12 16:53:49 -0300

    funcionando

--- js/init2.js
+++ js/init2.js
@@ -83,11 +83,11 @@
 		var y = $(window).scrollTop();
       var nav = $('#nav-wrap');
 
-	   if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
+	   if ( (y > h*.16) && (y < h) && ($(window).outerWidth() > 768 ) ) {
 	      nav.fadeOut('fast');
 	   }
       else {
-         if (y < h*.20) {
+         if (y < h*.16) {
             nav.removeClass('opaque').fadeIn('fast');
          }
          else {
