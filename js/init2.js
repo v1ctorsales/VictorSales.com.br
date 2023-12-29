d20977f4772a233070b4d3d113afafcfe38b5afa
Victor <victor.alves.sales@hotmail.com>
2023-12-29 18:52:48 -0300

    mudancas portfiolio

--- js/init2.js
+++ js/init2.js
@@ -180,6 +180,23 @@
 });
 
 
+(function () {
+   // add click animation to social links
+   var socials = document.querySelectorAll("[class*=social_lnk]");
+   var i = socials.length;
+   var cls = "-clicked";
+   while (i--) {
+     socials[i].addEventListener("click", function(e){
+       var lnk = e.target;
+       lnk.classList.add(cls);
+ 
+       // Allow time for animation to complete then remove
+       setTimeout(function() {
+         lnk.classList.remove(cls);
+       }, 300);
+     }, false);
+   }
+ })();
 
 
 
