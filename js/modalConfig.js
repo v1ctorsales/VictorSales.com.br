5b1d449012a16d449f00ff2e36f3d44cf2ec53dc
Victor <victor.alves.sales@hotmail.com>
2024-04-10 16:29:51 -0300

    fix botao de fechar configs

--- js/modalConfig.js
+++ js/modalConfig.js
@@ -1,9 +1,11 @@
 function fecharModalConfig(){
     document.querySelector("dialog").style.opacity = 0
     document.querySelector("dialog").classList.add("animated");
+    document.querySelector("dialog").style.top = '-50%'
     }
 
 function abrirModal(){
     document.querySelector("dialog").style.opacity = 0.2
     document.querySelector("dialog").classList.remove("animated");
+    document.querySelector("dialog").style.top = '50%'
 }
